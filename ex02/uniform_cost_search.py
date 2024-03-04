1aaae6c180200f38044f3bf0b931fe2b86f7d808
Ristafan <martin.faehnrich@gmail.com>
2024-03-04 08:52:16 +0100

    GPT generated code

--- ex02/uniform_cost_search.py
+++ ex02/uniform_cost_search.py
@@ -1,13 +1,33 @@
 import search_problem
+import heapq
 
 from search import Search, SearchNode
 
 
 class UniformCostSearch(Search):
-  name = "uniform-cost"
+  def __init__(self, search_problem, print_statistics=False):
+    super().__init__(search_problem, print_statistics)
+    self.name = "Uniform Cost"
 
   def search(self):
-    raise NotImplementedError
+    frontier = [(0, SearchNode(self.search_problem.initial_state, None, 0))]
+    explored = set()
+
+    while frontier:
+      cost, node = heapq.heappop(frontier)
+      if self.search_problem.is_goal(node.state):
+        return self.extract_path(node), node.g
+
+      if node.state not in explored:
+        explored.add(node.state)
+        self.expanded += 1
+        for action in self.search_problem.actions(node.state):
+          succ, action_cost = action
+          child_node = SearchNode(succ, node, node.g + action_cost)
+          self.generated += 1
+          heapq.heappush(frontier, (child_node.g, child_node))
+
+        return None, float('inf')
 
 
 if __name__ == "__main__":
